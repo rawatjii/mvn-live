2263b9cad7ede7b6a2bc1373d5ef6b1e14ca7b7d
Kuldeep chaudhary <kuldeep.chaudhary@gtftechnologies.com>
2025-01-30 12:47:10 +0530

    bangalore page

--- src/frontend/pages/MicroPageBangalore.jsx
+++ src/frontend/pages/MicroPageBangalore.jsx
@@ -1,300 +1,105 @@
-import React, { useEffect, useState, useRef } from "react";
-import MicroOverview from "../components/MicroPage/Overview";
-import MicroAmenities from "../components/MicroPage/Amenities";
-import MicroLocationMap from "../components/MicroPage/LocationMap";
-import Enquire from "../components/homepage/Enquire";
-import EnquireForm from "../components/homepage/EnquireForm";
-import Walkthrough from "../components/MicroPage/Walkthrough";
-import Footer from "../components/Footer";
-import DownloadBrochure from "../components/MicroPage/DownloadBrochure";
-import { gsap } from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-import ScrollSmoother from "gsap/ScrollSmoother";
-import { Helmet } from "react-helmet";
+import React, { useEffect, useState, useRef, Suspense, useMemo } from "react";
+
+// Load critical components normally
 import MicroHeader from "../components/MicroHeader";
-import ImagesGallery from "../components/MicroPage/ImagesGallery";
+import Footer from "../components/Footer";
 import ScrollTriggerFrames from "../components/MicroPage/ScrollTriggerFrames";
-import BangaloreElevationSection from "../components/MicroPage/bangalore/BangaloreElevationSection";
-import SliderTypology from "../components/MicroPage/bangalore/SliderTypology";
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
- 
-const MicroPageBangalore = ({ data, loadingCount, setLoadingCount }) => {
-  const [heroLoaded, setHeroLoaded] = useState(true);
-  const [newLoadingCount, setNewLoadingCount] = useState(
-    Math.floor(localStorage.getItem('count') || 0)
-  );
-  const [isPageLoaded, setIsPageLoaded] = useState(false);
-  const [peacockLoaded, setPeacockLoaded] = useState(false);
-  const [livingRoomLoaded, setLivingRoomLoaded] = useState(false);
-  const [partyLoaded, setPartyLoaded] = useState(false);
-  const [masterBedroomLoaded, setMasterBedroomLoaded] = useState(false);
-  const [typologyLoaded, setTypologyLoaded] = useState(false);
-  const smootherRef = useRef(null);
-  const sectionRefs = useRef({});
-  const {pageSections, projectName} = data;
+// Lazy load non-critical components
+const MicroOverview = React.lazy(() => import("../components/MicroPage/Overview"));
+const MicroAmenities = React.lazy(() => import("../components/MicroPage/Amenities"));
+const MicroLocationMap = React.lazy(() => import("../components/MicroPage/LocationMap"));
+const Enquire = React.lazy(() => import("../components/homepage/Enquire"));
+const EnquireForm = React.lazy(() => import("../components/homepage/EnquireForm"));
+const Walkthrough = React.lazy(() => import("../components/MicroPage/Walkthrough"));
+const DownloadBrochure = React.lazy(() => import("../components/MicroPage/DownloadBrochure"));
+const ImagesGallery = React.lazy(() => import("../components/MicroPage/ImagesGallery"));
+const BangaloreElevationSection = React.lazy(() => import("../components/MicroPage/bangalore/BangaloreElevationSection"));
+const SliderTypology = React.lazy(() => import("../components/MicroPage/bangalore/SliderTypology"));
 
+const MicroPageBangalore = ({ data }) => {
+  const sectionRefs = useRef([]);
   const [isHeaderFixed, setIsHeaderFixed] = useState(false);
-  const bannerRef = useRef(null);
 
-
-  const scrollToSection = (sectionKey) => {
-    const target = sectionRefs.current[sectionKey];
-    if (target && smootherRef.current) {
-      smootherRef.current.scrollTo(target, true);
-    }
-  };
-
-  useEffect(() => {
-    if (heroLoaded) {
-      smootherRef.current = ScrollSmoother.create({
-        wrapper: "#smooth-wrapper",
-        content: "#smooth-content",
-        smooth: 1.5,
-        effects: true,
-        smoothTouch: 1.4,
-      });
-    }
-    return () => {
-      if (smootherRef.current) {
-        smootherRef.current.kill();
-        smootherRef.current = null;
-      }
-    };
-  }, [heroLoaded]);
+  // Memoize data to avoid unnecessary re-renders
+  const memoizedData = useMemo(() => data, [data]);
 
   return (
     <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <MicroHeader data={memoizedData.header} isFixed={isHeaderFixed} />
 
-      
-      <Helmet>
-        <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag() { dataLayer.push(arguments); }
-          `}
-        </script>
-
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-P7MQ5KWWGL"
-        ></script>
-
-        <script>
-          {`
-            gtag('js', new Date());
-            gtag('config', 'G-P7MQ5KWWGL');
-          `}
-        </script>
-
-        {/* Google tag (gtag.js) */}
-
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-T7YNXS59XR"></script>
-
-        <script>
-          {`
-            gtag('js', new Date());
-
-            gtag('config', 'G-T7YNXS59XR');
-          `}
-        </script>
-
-        {/* conversion code */}
-
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11490416244"></script>
-
-        <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'AW-11490416244');
-          `}
-        </script>
-
-        {/* Meta Pixel Code */}
-
-        <script>
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '562105226581202');
-            fbq('track', 'PageView');
-            `}
-          </script>
-
-          <noscript>
-            {`
-              <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=562105226581202&ev=PageView&noscript=1" />
-            `}
-          </noscript>
-
-          <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11490416244"></script>
-          <script>
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'AW-11490416244');
-            `}
-          </script>
-
-          <script>
-              {`
-                <script type='text/javascript'>
-                window._tfa = window._tfa || [];
-                window._tfa.push({notify: 'event', name: 'page_view', id: 1787600});
-                !function (t, f, a, x) {
-                      if (!document.getElementById(x)) {
-                          t.async = 1;t.src = a;t.id=x;f.parentNode.insertBefore(t, f);
-                      }
-                }(document.createElement('script'),
-                document.getElementsByTagName('script')[0],
-                '//cdn.taboola.com/libtrc/unip/1787600/tfa.js',
-                'tb_tfa_script');
-              </script>
-              `}
-          </script>
-
-          <script>
-            {`
-              (function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"187169642", enableAutoSpaTracking: true};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");
-            `}
-          </script>
-
-      </Helmet>
-
-      <MicroHeader scrollToSection={scrollToSection} data={data.header} isFixed={ isHeaderFixed }/>
-      <div id="smooth-wrapper">
-        <div id="smooth-content">
-          
-          <div ref={bannerRef}>
+        <div>
+          {/* Hero Section */}
+          <div>
             <ScrollTriggerFrames 
-            data={data.micro_hero_section}
-            onLoadComplete={() => setHeroLoaded(true)}
-            onBannerExit={setIsHeaderFixed} 
-            isMainBanner={true} 
+              data={memoizedData.micro_hero_section}
+              onBannerExit={setIsHeaderFixed}
+              isMainBanner={true}
             />
           </div>
-          {/* Render other components only after Hero Section is loaded */}
 
-          <div ref={(el) => (sectionRefs.current.microOverview = el)}>
-          <MicroOverview data={data.overview} heroLoadedStatus={heroLoaded} /> {/*no isssue*/}
-          </div>
-          <BangaloreElevationSection/>
-
-          
-          <div
-            ref={(el) =>
-              (sectionRefs.current.Walkthrough = el)
-            }
-          >
-            <Walkthrough data={data.walkthrough}/>
+          {/* Other Sections */}
+          <div ref={(el) => (sectionRefs.current[0] = el)}>
+            <MicroOverview data={memoizedData.overview} />
           </div>
 
-          <div
-            ref={(el) =>
-              (sectionRefs.current.downloadBrochure = el)
-            }
-          >
+          <BangaloreElevationSection />
+
+          <div ref={(el) => (sectionRefs.current[1] = el)}>
+            <Walkthrough data={memoizedData.walkthrough} />
+          </div>
+
+          <div ref={(el) => (sectionRefs.current[2] = el)}>
             <DownloadBrochure />
           </div>
-          <div
-            ref={(el) =>
-              (sectionRefs.current.livingRoom = el)
-            }>
-            <ScrollTriggerFrames 
-            data={data.living_room}
-            onLoadComplete={() => setLivingRoomLoaded(true)}
-            />
-          </div>
-          <div 
-            ref={(el) =>
-              (sectionRefs.current.masterBedroom = el)
-            }>
-            <ScrollTriggerFrames 
-            data={data.masterBedroom}
-            onLoadComplete={() => setMasterBedroomLoaded(true)}
-            />
-          </div>
-          <div
-            ref={(el) =>
-              (sectionRefs.current.MicroLandscape = el)
-            }
-          >
-            <ImagesGallery data={data.landscape}/>
+
+          <div ref={(el) => (sectionRefs.current[3] = el)}>
+            <ScrollTriggerFrames data={memoizedData.living_room} />
           </div>
 
-          <div
-            ref={(el) =>
-              (sectionRefs.current.MicroElevation = el)
-            }
-          >
-            <ImagesGallery data={data.microElevation}/>
+          <div ref={(el) => (sectionRefs.current[4] = el)}>
+            <ScrollTriggerFrames data={memoizedData.masterBedroom} />
           </div>
 
-         <div
-            ref={(el) =>
-              (sectionRefs.current.MicroApartment = el)
-            }
-          >
-            <ImagesGallery data={data.microApartment}/>
+          <div ref={(el) => (sectionRefs.current[5] = el)}>
+            <ImagesGallery data={memoizedData.landscape} />
           </div>
 
-          <div
-            ref={(el) =>
-              (sectionRefs.current.MicroAmenities = el)
-            }>
-            <MicroAmenities section_data={data.amenities} />
+          <div ref={(el) => (sectionRefs.current[6] = el)}>
+            <ImagesGallery data={memoizedData.microElevation} />
           </div>
 
-          <div
-            ref={(el) =>
-              (sectionRefs.current.MicroTypology = el)
-            }
-          >
-            <SliderTypology data={data.typologies} onLoadComplete={() => setTypologyLoaded(true)} />
+          <div ref={(el) => (sectionRefs.current[7] = el)}>
+            <ImagesGallery data={memoizedData.microApartment} />
           </div>
 
-          <div
-            ref={(el) =>
-              (sectionRefs.current.MicroLocationMap = el)
-            }
-          >
-            <MicroLocationMap
-              data={data.locationAdvantage}
-            />
+          <div ref={(el) => (sectionRefs.current[8] = el)}>
+            <MicroAmenities section_data={memoizedData.amenities} />
           </div>
 
-          <div
-            className="container-fluid micro_footer"
-            ref={(el) => (sectionRefs.current.Enuqiry = el)}
-          >
-            <div className="row ">
+          <div ref={(el) => (sectionRefs.current[9] = el)}>
+            <SliderTypology data={memoizedData.typologies} />
+          </div>
+
+          <div ref={(el) => (sectionRefs.current[10] = el)}>
+            <MicroLocationMap data={memoizedData.locationAdvantage} />
+          </div>
+
+          <div className="container-fluid micro_footer" ref={(el) => (sectionRefs.current[11] = el)}>
+            <div className="row">
               <div className="col-sm-6 px-0">
                 <Enquire />
               </div>
               <div className="col-sm-6 px-0">
-                <EnquireForm
-                  projectName={"MVN Aeroone"}
-                />
+                <EnquireForm projectName={"MVN Aeroone"} />
               </div>
             </div>
           </div>
 
           <Footer />
         </div>
-      </div>
-
-      {/* <ScrollTop /> */}
+      </Suspense>
     </>
   );
 };
