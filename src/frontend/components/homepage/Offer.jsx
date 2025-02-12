7ba7c76e5a3745298ed1fb61bee59e87daeb717b
sandeep~ <sandeep@gtftechnologies.com>
2025-02-12 18:24:54 +0530

    hide loop for offer video

--- src/frontend/components/homepage/Offer.jsx
+++ src/frontend/components/homepage/Offer.jsx
@@ -6,8 +6,8 @@ const Offer = React.memo(({clickHandler})=>{
   return(
     <div className="offers_section">
       <div className="single" role="presentation" aria-label="Play offer video" style={{cursor:'pointer'}} onClick={()=>clickHandler(false)}>
-        <video src={CONFIG.IMAGE_URL + "offer/offer1.mp4"} muted  aria-label="Play offer video promotion" className="img-fluid d-none d-md-block" playsInline autoPlay loop />
-        <video src={CONFIG.IMAGE_URL + "offer/offer1_sm.mp4"} muted  aria-label="Play offer video promotion mobile version" className="img-fluid d-md-none" playsInline autoPlay loop />
+        <video src={CONFIG.IMAGE_URL + "offer/offer1.mp4"} muted  aria-label="Play offer video promotion" className="img-fluid d-none d-md-block" playsInline autoPlay />
+        <video src={CONFIG.IMAGE_URL + "offer/offer1_sm.mp4"} muted  aria-label="Play offer video promotion mobile version" className="img-fluid d-md-none" playsInline autoPlay />
       </div>
     </div>
   )
