26f3977a1de100e1b0e8e213ca1901e79813365d
sandeep~ <sandeep@gtftechnologies.com>
2025-02-13 17:23:57 +0530

    changes

--- src/test/homepage.test.jsx
+++ src/test/homepage.test.jsx
@@ -1,37 +1,43 @@
 import {render, screen, fireEvent} from '@testing-library/react';
 import Hero from '../frontend/components/homepage/Hero';
 import {MemoryRouter} from 'react-router-dom';
+import userEvent from "@testing-library/user-event";
 
 // Ensure VITE_APP_URL is set in the test environment
-import.meta.env.VITE_APP_URL = "/";
+// import.meta.env.VITE_APP_URL = "/";
 
 test('testing homepage hero section', async()=>{
+    const user = userEvent.setup();
     render(
         <MemoryRouter>
             <Hero />
         </MemoryRouter>
     );
-
-    screen.debug();
-
     // check if hero image is present
     const imgElement = screen.getByAltText('Hero Banner');
     expect(imgElement).toBeInTheDocument();
 
     // check if there is a tag which will redirect to aeroone gurgaon page
-    const linkElement = await screen.findByRole('link');
+    const linkElement = screen.getAllByRole('link').find((link)=>(
+        link.getAttribute('rel') === 'noopener noreferrer'
+    ))
 
     // Check if the link opens in a new tab
+    expect(linkElement).toBeInTheDocument();
     expect(linkElement).toHaveAttribute("target","_blank");
     expect(linkElement).toHaveAttribute("href","/aeroone-gurgaon");
 
     // click
-    await userEvent.click(linkElement);
+    await user.click(linkElement);
+
+    const heroHeading = document.querySelector('.slider-heading');
+    expect(heroHeading).toBeInTheDocument();
+
+    // 
 
     // check if there is button and clickable
     const button = screen.getByText('Click Here');
     fireEvent.click(button);
 
-    // check if button click redirect to new page
     
 })
