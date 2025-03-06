e858828a1a71b620c6566c9a9fecdf16974b0604
sandeep~ <sandeep@gtftechnologies.com>
2025-03-06 18:18:30 +0530

    add sizes in micropage

--- src/frontend/pages/micro/mvn-aeroone-gurgaon1/Index.jsx
+++ src/frontend/pages/micro/mvn-aeroone-gurgaon1/Index.jsx
@@ -79,6 +79,7 @@ export const data = {
     // location: "Gurugram",
     extra: "AN EPITOME OF GLOBAL LUXURY NOW IN GURUGRAM",
     desc: "Gurugram is set to redefine luxury living with the launch  of MVN ultra-luxurious 5.5 BHK apartments, offering unmatched opulence. Combining the grandeur of Dubai and New York, these residences feature cutting-edge designs, premium amenities, and breathtaking views. Experience an elite lifestyle with sophistication and comfort at its pinnacle, like never before!",
+    counterHeading:true,
     // desc: [
     //   'Luxury Reimagined. Elegance Reborn.',
     //   'Step into a world of unparalleled luxury, where every detail blends sophistication with style. Relish in tailored interiors, exceptional amenities, and panoramic views. Transform your lifestyle with an extraordinary living experience beyond compare.',
