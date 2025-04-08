2b539093bf8fd603aad32b9d86d44716487f4807
sandeep~ <sandeep@gtftechnologies.com>
2025-04-08 16:56:27 +0530

    bug

--- src/admin/components/dashboard/microsite/Basic.jsx
+++ src/admin/components/dashboard/microsite/Basic.jsx
@@ -6,87 +6,23 @@ import { useParams } from "react-router-dom";
 
 // Simulated backend response
 const formConfig = [
-  {
-    sectionName: "Meta Micro Site",
-    sectionApi: "meta-micro-site",
-    visible: "true",
-    fields: {
-      title: { visible: "true",name : "meta_name", label: "Meta Title", isLeft: true },
-      title: { visible: "true", label: "Meta Keyword", isLeft: true },
-      image: { visible: "true", label: "Main Image", isLeft: true },
-      alternative_image: { visible: "true", label: "Alternative Image", isLeft: true },
-      description: { visible: "true", isLeft: true },
-    },
-  },
-  {
-    sectionName: "About overview",
-    sectionApi: "about-overview",
-    visible: "true",
-    fields: {
-      title: { visible: "true", label: "Overview Title" },
-      alt_tag: { visible: "true", label: "Alt Tag" },
-      image: { visible: "true", label: "Overview Image" },
-      alternative_image: { visible: "true", label: "Alt Image" },
-      description: { visible: "true", label: "Overview Description" },
-    },
-  },
-  {
-    sectionName: "About gallery",
-    sectionApi: "about-gallery",
-    visible: "true",
-    fields: {
-      title: { visible: "true", label: "Overview Title" },
-      alt_tag: { visible: "true", label: "Alt Tag" },
-      image: { visible: "true", label: "Overview Image" },
-      alternative_image: { visible: "true", label: "Alt Image" },
-      description: { visible: "true", label: "Overview Description" },
-    },
-  },
-];
+  { visible: "true",name : "meta_name", label: "Meta Title", isLeft: true },
+  { visible: "true",name : "title ", label: "Meta Title", isLeft: true },
+  { visible: "true",name : "ame", label: "Meta Title", isLeft: true },
+]
 
 const BasicMicroSite = () => {
-  const { pageName } = useParams(); 
-
-  const handleSectionSubmit = (sectionApi, data) => {
-    console.log(`🔽 Data from section [${sectionApi}] is already FormData`);
-    console.log(`🟡 FormData content for [${sectionApi}]:`);
-    for (let [key, value] of data.entries()) {
-      console.log(`${key}:`, value instanceof File ? value.name : value);
-    }
-  
-    const endpoint = "https://dummyjson.com/test"; 
-  
-    fetch(endpoint, {
-      method: "POST",
-      body: data,
-    })
-      .then((res) => res.text()) // Webhook.site returns plain text
-      .then((resData) => {
-        console.log(`✅ Data submitted successfully`, resData);
-      })
-      .catch((err) => {
-        console.error(`❌ Error in submit:`, err);
-      });
-  };
-
-
-  return (
+turn (
     <CustomSection customClass="d-block">
       <div class="left-area">
-        {formConfig
-          .filter((section) => section.visible)
-          .map((section) => (
-            <MicroBox key={section.sectionName}>
-              <CustomTitle title={section.sectionName} />
+        
+      <MicroBox>
+              <CustomTitle title={'test'} />
               <CustomForm
-                formType="block"
-                fieldVisibility={section.fields}
-                onSubmit={(data) =>
-                  handleSectionSubmit(section.sectionApi, data)
-                }
+                isBanner={false}
+                dynamicFields={formConfig}
               />
             </MicroBox>
-          ))}
       </div>
     </CustomSection>
   );
