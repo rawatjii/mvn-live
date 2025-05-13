1499232f3a6508c3427ebf4e97cc68201ffaebbe
Hritik <hritik.kumar@gmail.com>
2025-05-13 15:42:45 +0530

    CHANGES

--- src/admin/components/dashboard/microsite/Elevation.jsx
+++ src/admin/components/dashboard/microsite/Elevation.jsx
@@ -62,7 +62,7 @@ const Elevation = () => {
 
   const handleCreateMain = async (formData) => {
     try {
-      formData.append("is_type","image")
+      formData.append("is_type",locationPathname)
 
       await createElevationItem(formData);
       console.log("Elevation item created successfully");
