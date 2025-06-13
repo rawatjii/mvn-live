173269f57b56c298b5d8acc1aa3c966e6f5d311d
sandeep~ <sandeep@gtftechnologies.com>
2025-06-13 17:39:31 +0530

    update sidebar icons

--- src/admin/components/ContentLayout/microSidebar/MicroSidebar.jsx
+++ src/admin/components/ContentLayout/microSidebar/MicroSidebar.jsx
@@ -24,34 +24,35 @@ import { VITE_APP_URL } from "../../../../config/config";
 import generateApi from "../../../api/generateApi";
 import useCrud from "../../../hooks/useCrud";
 
-// const navItems = [
-//   { to: "", icon: <FaRegFileAlt fontSize={24} />, description: "Basic", exact: true },
-//   { to: "banner", icon: <IoIosImage fontSize={28} />, description: "Banner" },
-//   { to: "overview", icon: <IoMdInformationCircleOutline fontSize={28} />, description: "Overview" },
-//   { to: "elevation", icon: <BsBuildingUp fontSize={24} />, description: "Elevation" },
-//   { to: "walkthrough", icon: <MdVideoSettings fontSize={24} />, description: "Walkthrough" },
-//   { to: "threesixtyview", icon: <TbView360Number fontSize={24} />, description: "360°" },
-//   { to: "livingroom", icon: <MdOutlineLiving fontSize={24} />, description: "Living Room" },
-//   { to: "party", icon: <LuPartyPopper fontSize={24} />, description: "Party" },
-//   { to: "masterbedroom", icon: <MdBedroomParent fontSize={24} />, description: "MasterBedroom" },
-//   { to: "consultant", icon: <FaUserCog fontSize={24} />, description: "Consultant" },
-//   { to: "landscape", icon: <BiLandscape fontSize={24} />, description: "Land Scape" },
-//   { to: "sm-elevation", icon: <HiOutlineBuildingStorefront fontSize={24} />, description: "SmallElevation" },
-//   { to: "apartment", icon: <PiBuildingApartment fontSize={24} />, description: "Apartment" },
-//   { to: "construction", icon: <MdOutlineConstruction fontSize={24} />, description: "Construction" },
-//   { to: "amenities", icon: <MdOutlineConstruction fontSize={24} />, description: "Amenities" },
-//   { to: "typologies", icon: <MdOutlineFeaturedPlayList fontSize={24} />, description: "Typologies" },
-//   { to: "floor-plans", icon: <PiStrategyBold fontSize={24} />, description: "Floor Plan" },
-//   { to: "location-map", icon: <MdOutlineConstruction fontSize={24} />, description: "Location Map" },
-//   { to: "mvn-mall", icon: <MdShareLocation fontSize={24} />, description: "MVN Mall" },
-//   { to: "connection-mvn-mall", icon: <MdStoreMallDirectory fontSize={24} />, description: "Connection MVN Mall" },
-// ];
+const navItems = [
+  // { to: "", icon: <FaRegFileAlt fontSize={24} />, description: "Basic", exact: true },
+  // { to: "banner", icon: <IoIosImage fontSize={28} />, description: "Banner" },
+  { to: "overview", icon: <IoMdInformationCircleOutline fontSize={28} />, description: "Overview" },
+  { to: "overview", icon: <IoMdInformationCircleOutline fontSize={28} />, description: "Overview" },
+  { to: "elevation", icon: <BsBuildingUp fontSize={24} />, description: "Elevation" },
+  { to: "walkthrough", icon: <MdVideoSettings fontSize={24} />, description: "Walkthrough" },
+  { to: "threesixtyview", icon: <TbView360Number fontSize={24} />, description: "360°" },
+  { to: "livingroom", icon: <MdOutlineLiving fontSize={24} />, description: "Living Room" },
+  { to: "party", icon: <LuPartyPopper fontSize={24} />, description: "Party" },
+  { to: "masterbedroom", icon: <MdBedroomParent fontSize={24} />, description: "MasterBedroom" },
+  { to: "consultant", icon: <FaUserCog fontSize={24} />, description: "Consultant" },
+  { to: "landscape", icon: <BiLandscape fontSize={24} />, description: "Land Scape" },
+  { to: "sm-elevation", icon: <HiOutlineBuildingStorefront fontSize={24} />, description: "SmallElevation" },
+  { to: "apartment", icon: <PiBuildingApartment fontSize={24} />, description: "Apartment" },
+  { to: "construction", icon: <MdOutlineConstruction fontSize={24} />, description: "Construction" },
+  { to: "amenities", icon: <MdOutlineConstruction fontSize={24} />, description: "Amenities" },
+  { to: "typologies", icon: <MdOutlineFeaturedPlayList fontSize={24} />, description: "Typologies" },
+  { to: "floor-plans", icon: <PiStrategyBold fontSize={24} />, description: "Floor Plan" },
+  { to: "location-map", icon: <MdOutlineConstruction fontSize={24} />, description: "Location Map" },
+  { to: "mvn-mall", icon: <MdShareLocation fontSize={24} />, description: "MVN Mall" },
+  { to: "connection-mvn-mall", icon: <MdStoreMallDirectory fontSize={24} />, description: "Connection MVN Mall" },
+  { to: "connection-mvn-mall", icon: <MdStoreMallDirectory fontSize={24} />, description: "Connection MVN Mall" },
+];
 
 const MicroSidebar = () => {
   const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
   const toggleSidebar = () => setSidebarIsOpen(!sidebarIsOpen);
 
-  const [navItems, setNavItems] = useState([]);
   const { project_id } = useParams();
   const [searchParams] = useSearchParams();
   const themeId = searchParams.get("theme");
@@ -124,7 +125,7 @@ const MicroSidebar = () => {
                       }
                       {...triggerHandler}
                     >
-                      <div className="icon"><FaRegFileAlt fontSize={24} /></div>
+                      <div className="icon"><IoIosImage fontSize={28} /></div>
                       <div className="description">Banner</div>
                     </NavLink>
                   )}
@@ -132,7 +133,7 @@ const MicroSidebar = () => {
             {data?.map((item, index) => {
               if (!project_id && index !== 0) return null;
 
-              if(item.slug === 'location-advantage') return null;
+              if(item.slug === 'location-advantage' || item.slug === 'sizes') return null;
 
               return (
                 <OverlayTrigger
@@ -151,7 +152,8 @@ const MicroSidebar = () => {
                       }
                       {...triggerHandler}
                     >
-                      <div className="icon"><FaRegFileAlt fontSize={24} /></div>
+                      
+                      <div className="icon">{navItems[index]?.icon}</div>
                       <div className="description">{item.name}</div>
                     </NavLink>
                   )}
