11b1316d1383e980c33cfa3a49c83955ce30b0e2
sandeep~ <sandeep@gtftechnologies.com>
2025-06-18 11:18:19 +0530

    update

---
+++ src/admin/components/dashboard/ProjectLists.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ProjectLists = ()=>{
+  return(
+    <h1>testing</h1>
+  )
+}
+
+export default ProjectLists 
--- src/admin/components/media-centre/PressRelease.jsx
+++ src/admin/components/media-centre/PressRelease.jsx
@@ -44,6 +44,10 @@ const PressRelease = () => {
   
     const handleCreate = (formData) => {
       formData.append("type","press")
+      // console.log('handlecreate', formData);
+      // for (const [key, value] of formData.entries()) {
+      //   console.log(`${key}: ${value}`);
+      // }
       createItem(formData)
     };
     // const handleEdit = (row) => updateItem(row.id, row);
@@ -54,6 +58,9 @@ const PressRelease = () => {
     };
   
     const handleEditSubmit = (formData) => {
+      // for (const [key, value] of formData.entries()) {
+      //   console.log(`${key}: ${value}`);
+      // }
       editItem(editModalData.id, formData);
     };
   
