900278ee23098f9207fe6c652b65cc10e44e11e3
sandeep~ <sandeep@gtftechnologies.com>
2025-06-24 12:29:40 +0530

    update

--- src/frontend/components/ApprovedSitePlans.jsx
+++ src/frontend/components/ApprovedSitePlans.jsx
@@ -14,16 +14,19 @@ const sitePlans = [
       src: `${CONFIG.API_URL}images/mediacenter/site_plans/principle_approval_letter.pdf`,
       slug:`principle_approval_letter`,
       title: "In-Principle Approval Letter",
+      date:'14 June, 2025'
     },
     {
       src: `${CONFIG.API_URL}images/mediacenter/site_plans/approved_site_plan_ph1.pdf`,
       slug:`approved_site_plan_ph1`,
       title: "Approved Site Plan for Phase-I",
+      date:'14 June, 2025'
     },
     {
       src: `${CONFIG.API_URL}images/mediacenter/site_plans/approved_site_plan_ph1_ph2.pdf`,
       slug:`approved_site_plan_ph1_ph2`,
-      title: "In-Principle Approved Site Plan for Phase-I & II",
+      title: "In-Principle Approved Site Plan for Phase-II & III",
+      date:'14 June, 2025'
     },
 ];
 
@@ -43,22 +46,25 @@ function ApprovedSitePlans() {
                                 <Viewer fileUrl={item.src} />
                             </div>
                         </Worker> */}
-                        {/* <article className="awa_card awa_shadow app_site_plan_box">
+                        <article className="awa_card awa_shadow app_site_plan_box">
                             <div>
                                 <img src={CONFIG.API_URL + 'images/icons/pdf_icon.svg'} alt={item.title} className="pressRelease-Img" style={{objectFit:'contain'}} />
                             </div>
                             <div>
                                 <h6 className="title">{item.title}</h6>
-                                <Link className="text-capitalize" target="_blank" to={import.meta.env.VITE_APP_URL + `pdf/${item.slug}`}>View Full PDF</Link>
+                                <div className="awa_posted d-md-flex justify-content-between align-items-center mt-1 mt-md-3 mb-0">
+                                    <span className="text-capitalize"><time>{item.date}</time></span>
+                                    <Link className="text-capitalize" target="_blank" to={import.meta.env.VITE_APP_URL + `pdf/${item.slug}`}>View Full PDF</Link>
+                                </div>
                             </div>
-                        </article> */}
-                         <article className="awa_card awa_shadow d-block">
+                        </article>
+                         {/*<article className="awa_card awa_shadow d-block">
                             <div className="d-block">
                                 <h6 className="title">{item.title}</h6>
                                 <Link className="text-capitalize" target="_blank" to={import.meta.env.VITE_APP_URL + `pdf/${item.slug}`}>View Full PDF</Link>
-                                {/* <p className="text-capitalize" onClick={() => window.open(item.src, "_blank")}>View Full PDF</p> */}
+                                /~ <p className="text-capitalize" onClick={() => window.open(item.src, "_blank")}>View Full PDF</p> ~/
                             </div>
-                        </article>
+                        </article>*/}
                     </div>
                 ))}
                 
