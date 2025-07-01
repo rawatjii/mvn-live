4cc7a300531dbc23123cfb22c1a2358a77ff8719
sandeep~ <sandeep@gtftechnologies.com>
2025-07-01 16:40:24 +0530

    update lazy load component

--- src/common/LazyLoadComponent.jsx
+++ src/common/LazyLoadComponent.jsx
@@ -1,33 +1,41 @@
 import React, { useEffect } from "react";
-import {useInView} from 'react-intersection-observer';
+import { useInView } from "react-intersection-observer";
 
-const LazyLoadComponent = ({children, margin, debugName, smootherRef})=>{
-  const {ref, inView} = useInView({
-    triggerOnce:true,
-    threshold:0,
-    rootMargin:margin ? margin : '100px',
-    root: document.querySelector('#smooth-wrapper'),
+const LazyLoadComponent = ({ children, margin, debugName, smootherRef }) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0,
+    rootMargin: margin ? margin : "100px",
+    root: document.querySelector("#smooth-wrapper"),
   });
 
   useEffect(() => {
     if (inView) {
-      const scrollY = window.scrollY || smootherRef?.current?.offset() || 0;
-      setTimeout(() => {
-        ScrollTrigger.refresh();
-        if (smootherRef?.current) {
-          smootherRef.current.scrollTo(scrollY, false);
-        } else {
-          window.scrollTo(0, scrollY);
-        }
-      }, 100);
+      const scrollY =
+        window.scrollY ||
+        (smootherRef &&
+        smootherRef.current &&
+        typeof smootherRef.current.offset === "function"
+          ? smootherRef.current.offset()
+          : 0);
+      const timeoutId = setTimeout(() => {
+        try {
+          if (typeof ScrollTrigger !== "undefined") {
+            ScrollTrigger.refresh();
+          }
+          if (smootherRef?.current) {
+            smootherRef.current.scrollTo(scrollY, false);
+          } else {
+            window.scrollTo(0, scrollY);
+          }
+        } catch (error) {}
+      }, 200);
+
+      return () => clearTimeout(timeoutId);
     }
   }, [inView, debugName, smootherRef]);
 
-  return(
-    <div ref={ref}>
-      {inView && children}
-    </div>
-  )
-}
+  return <div ref={ref}>{inView && children}</div>;
+};
 
 export default LazyLoadComponent;
