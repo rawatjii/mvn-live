52421034563a798e28dec83f0d0293a6c4c717d4
kuldeep chaudhary <you@example.com>
2025-08-05 17:00:03 +0530

    center

--- src/frontend/components/ContactInfo.jsx
+++ src/frontend/components/ContactInfo.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
-import { API_URL } from '../../config/config'
 
 const ContactInfo = () => {
   return (
     <section className='Contact-info'>
         <div className="container">
-    <div className='row gy-3'>
-        <div className="col-12 col-md-6">
+    <div className='row gy-3 d-flex justify-content-center'>
+        <div className="col-12 col-md-6 col-xl-4">
             <div className="card">
-                <div><img src="/icons/user.png" alt="user" /> <span className='name'>Vidhi Negi</span></div>
+                <div><span className='name'>Vidhi Negi</span></div>
                 <div><img src="/icons/employee.png" alt="designation" /><span> DGM Sales</span></div>
                 <div><img src="/icons/call.png" alt="call icon" /> <a href="tel:+919311051426">(+91) 9311051426</a></div>
                 <div><img src="/icons/email.png" alt="main icon" /> <a href="mailto:smtm@mvninfrastructure.com">smtm@mvninfrastructure.com</a></div>
                 
             </div>
         </div>
-        <div className="col-12 col-md-6">
+        <div className="col-12 col-md-6 col-xl-4">
             <div className="card">
-                <div> <img src="/icons/user.png" alt="user" /> <span className='name'>Archi Rajpal</span></div>
+                <div> <span className='name'>Archi Rajpal</span></div>
                 <div><img src="/icons/employee.png" alt="designation" /><span> DGM Sales</span></div>
                 <div><img src="/icons/call.png" alt="call icon" /> <a href="tel:+919870101385">(+91) 9870101385</a></div>
                 <div><img src="/icons/email.png" alt="main icon" /> <a href="mailto:smto@mvninfrastructure.com">smto@mvninfrastructure.com</a></div>
