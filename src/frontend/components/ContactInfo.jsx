2020754bb08587f10c25e121f587db188cb14de4
kuldeep chaudhary <you@example.com>
2025-08-05 16:45:59 +0530

    FINEAL

--- src/frontend/components/ContactInfo.jsx
+++ src/frontend/components/ContactInfo.jsx
@@ -9,7 +9,7 @@ const ContactInfo = () => {
         <div className="col-12 col-md-6">
             <div className="card">
                 <div><img src="/icons/user.png" alt="user" /> <span className='name'>Vidhi Negi</span></div>
-                <div><img src="/public/icons/employee.png" alt="designation" /><span> DGM Sales</span></div>
+                <div><img src="/icons/employee.png" alt="designation" /><span> DGM Sales</span></div>
                 <div><img src="/icons/call.png" alt="call icon" /> <a href="tel:+919311051426">(+91) 9311051426</a></div>
                 <div><img src="/icons/email.png" alt="main icon" /> <a href="mailto:smtm@mvninfrastructure.com">smtm@mvninfrastructure.com</a></div>
                 
@@ -18,7 +18,7 @@ const ContactInfo = () => {
         <div className="col-12 col-md-6">
             <div className="card">
                 <div> <img src="/icons/user.png" alt="user" /> <span className='name'>Archi Rajpal</span></div>
-                <div><img src="/public/icons/employee.png" alt="designation" /><span> DGM Sales</span></div>
+                <div><img src="/icons/employee.png" alt="designation" /><span> DGM Sales</span></div>
                 <div><img src="/icons/call.png" alt="call icon" /> <a href="tel:+919870101385">(+91) 9870101385</a></div>
                 <div><img src="/icons/email.png" alt="main icon" /> <a href="mailto:smto@mvninfrastructure.com">smto@mvninfrastructure.com</a></div>
                 
