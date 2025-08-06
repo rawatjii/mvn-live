0684c6842eddc7d7af3f3c881beda8fa59b265c6
kuldeep chaudhary <you@example.com>
2025-08-06 15:34:31 +0530

    heading

--- src/frontend/components/ContactInfo.jsx
+++ src/frontend/components/ContactInfo.jsx
@@ -4,6 +4,10 @@ const ContactInfo = () => {
   return (
     <section className='Contact-info'>
         <div className="container">
+            <div className='contact-info-title'>
+            <h2>Step into luxury — Digitally or in person. Connect now</h2>
+            </div>
+
     <div className='row gy-3 d-flex justify-content-center'>
         <div className="col-12 col-md-6 col-xl-4">
             <div className="card">
