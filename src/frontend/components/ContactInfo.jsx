34b7d3ee110070c92f518c9c252acbdd4cd176c0
kuldeep chaudhary <you@example.com>
2025-08-21 14:27:55 +0530

    comit

--- src/frontend/components/ContactInfo.jsx
+++ src/frontend/components/ContactInfo.jsx
@@ -4,13 +4,9 @@ const ContactInfo = () => {
   return (
     <section className='Contact-info contact-us-section '>
         <div className="container">
-            <div className='contact-info-title'>
-            <h2>Begin your Project Exploration with us, your journey begins here. </h2>
-            <p className='detail_title mt-4'>Call our Ultra Luxury Sales Specialist</p>
-            </div>
 
     <div className='row gy-3 d-flex justify-content-center'>
-        <div className="col-12 col-md-6 col-xl-4">
+        <div className="col-12 col-md-6 col-xl-3">
             <div className="card">
                 <div><span className='name'>Vidhi Negi</span></div>
                 <div><img src="/icons/employee.png" alt="designation" /><span> DGM Sales</span></div>
@@ -19,7 +15,13 @@ const ContactInfo = () => {
                 
             </div>
         </div>
-        <div className="col-12 col-md-6 col-xl-4">
+        <div className="col-12 col-md-6 col-xl-6">
+            <div className='contact-info-title'>
+            <h2>Begin your Project Exploration with us, your journey begins here. </h2>
+            <p className='detail_title mt-4'>Call our Ultra Luxury Sales Specialist</p>
+            </div>
+        </div>
+        <div className="col-12 col-md-6 col-xl-3">
             <div className="card">
                 <div> <span className='name'>Archi Rajpal</span></div>
                 <div><img src="/icons/employee.png" alt="designation" /><span> DGM Sales</span></div>
