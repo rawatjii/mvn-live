20c3c81198e956f5b7181f63064d2ab9ad8388d9
Hritik <hritik.kumar@gtftechnologies.com>
2025-09-09 16:27:21 +0530

    changes

--- src/frontend/pages/Micro.jsx
+++ src/frontend/pages/Micro.jsx
@@ -398,8 +398,8 @@ useEffect(() => {
   };
 
   if (loading || sectionLoading) return renderLoadingScreen();
-  if (!project) return <PageNotFound />;
   if (!loading && project?.length === 0) return <div className="text-center py-5">No records found!!</div>;
+  if (!project) return <PageNotFound />;
 
   return (
     <>
