614cf70db351f50100600415e35f969818f4f088
Hritik <hritik.kumar@gtftechnologies.com>
2025-09-09 19:17:07 +0530

    changes

--- src/frontend/components/MicroPage/Hero/Index.jsx
+++ src/frontend/components/MicroPage/Hero/Index.jsx
@@ -47,17 +47,17 @@ const HeroSection = ({ projectId, onBannerExit, isMainBanner, projectName }) => 
       return () => player.off("play");
     }
   }, [vimeoPlayer, banner]);
-
-  useEffect(()=>{
-    console.log(banner,"banner")
-  },[banner])
-  if (loading) {
-    const loaderImage = projectName?.includes('aeroone-gurgaon')
+ const loaderImage = projectName?.includes('aeroone-gurgaon')
       ? `${API_URL}loader/homepage_loading${window.innerWidth < 768 ? '_sm' : ''}.webp`
       : projectName?.includes('mvn-mall')
       ? `${API_URL}loader/mvnMall_loader${window.innerWidth < 768 ? '_sm' : ''}.webp`
       : undefined;
 
+  useEffect(()=>{
+    console.log(banner,"banner")
+  },[banner])
+  if (loading) {
+   
     return (
       <div className="loading_screen" style={{ position: "relative" }}>
         {loaderImage && <img src={loaderImage} alt="loading screen" className="img-fluid w-100" />}
@@ -99,7 +99,7 @@ const HeroSection = ({ projectId, onBannerExit, isMainBanner, projectName }) => 
           {!isVideoPlaying && (
             <img
               src={projectName?.includes('aeroone-gurgaon')
-                ? `${API_URL}images/aero-gurgaon/hero/hero_loader${window.innerWidth < 768 ? '_sm' : ''}.webp`
+                ? `${loaderImage}`
                 : undefined}
               alt={alt || "Loading video..."}
               style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", objectFit: "cover", zIndex: 1 }}
