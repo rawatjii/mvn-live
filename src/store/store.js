ab794d90b4cf4d0d3f7fad95ffd64baaae3fa685
Hritik <hritik.kumar@gtftechnologies.com>
2025-09-11 16:36:21 +0530

    frontend homepage fixed

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import sideMenuReducer from '../redux/adminSidebarSlice';
 import blogsReducer from '../redux/blogsSlice';
 import commonReducer from '../redux/commonSlice';
@@ -8,54 +6,19 @@ import bannerReducer from '../redux/bannerSlice';
 import projectReducer from '../redux/projectDataSlice';
 import sectionReducer from '../redux/sectionDataSlice';
 import homeReducer from '../redux/homepageSlice';
-import layoutReducer from '../redux/layoutSlice'
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['banner', 'section'],
-  transforms: [
-    {
-      in: (state) => ({
-        ...state,
-        banner: {
-          ...state.banner,
-          loading: false,
-          error: null,
-        },
-        section: {
-          ...state.sectionData,
-          loading: false,
-          error: null,
-        },
-      }),
-      out: (state) => state,
-    },
-  ],
-};
-
-const persistedBannerReducer = persistReducer(persistConfig, bannerReducer);
-const persistedSectionReducer = persistReducer(persistConfig, sectionReducer);
+import layoutReducer from '../redux/layoutSlice';
 
 const store = configureStore({
   reducer: {
     adminSideMenu: sideMenuReducer,
     blogs: blogsReducer,
     commonState: commonReducer,
-    banner: persistedBannerReducer,
+    banner: bannerReducer,
     project: projectReducer,
     home: homeReducer,
-    section: persistedSectionReducer,
-    layout:layoutReducer
+    section: sectionReducer,
+    layout: layoutReducer
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
-        ignoredPaths: ['banner.error', 'section.error'], 
-      },
-    }),
-  devTools: process.env.NODE_ENV !== 'production',
 });
 
-export const persistor = persistStore(store);
 export default store;
